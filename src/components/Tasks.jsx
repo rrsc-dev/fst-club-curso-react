34bbd227115f8a71fd861ba24360f42a7728def4
rrsc-dev <reis.rrsc@outlook.com>
2026-02-03 11:33:52 -0300

    ajuste nome da função onDeleteTaskClick

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,12 @@
 import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
-  console.log(props);
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
+            onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 text-left text-white p-2 rounded-md w-full ${task.isCompleted ? "line-through" : ""}`}
           >
             {task.title}
@@ -16,7 +15,7 @@ function Tasks(props) {
             <ChevronRightIcon />
           </button>
           <button
-            onClick={() => props.onDeleteTaskClick(task.id)}
+            onClick={() => onDeleteTaskClick(task.id)}
             className="bg-slate-400 p-2 rounded-md text-white"
           >
             <TrashIcon />
