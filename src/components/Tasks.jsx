9d3fb81981c567d3decc194534ec20ef7c0bd7d1
rrsc-dev <reis.rrsc@outlook.com>
2026-02-03 13:56:36 -0300

    add função onSeeDetailsClick

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,13 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    navigate(`/task?title=${task.title}&description=${task.description}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -11,7 +18,10 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
           >
             {task.title}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button
+            onClick={() => onSeeDetailsClick(task)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <ChevronRightIcon />
           </button>
           <button
