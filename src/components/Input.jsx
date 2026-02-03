d514e7f1a31e851d495a6d0f14fe35b658b24bdc
rrsc-dev <reis.rrsc@outlook.com>
2026-02-03 16:00:09 -0300

    spread atributos do input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,8 @@
 function Input(props) {
   return (
     <input
-      type={props.type}
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={props.onChange}
       className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      {...props}
     />
   );
 }
