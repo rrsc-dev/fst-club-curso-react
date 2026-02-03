51be9d799bed1ea45701b9399e0c1d0c6a8e8c1a
rrsc-dev <reis.rrsc@outlook.com>
2026-02-03 09:59:13 -0300

    add: funções excluir e concluir tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
@@ -40,12 +41,59 @@ import Tasks from "./components/Tasks";
 // import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Loren ipsum dolor sit amet, consectetur adipiscing elit.",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Estudar React 2",
+      description: "Loren ipsum dolor sit amet, consectetur adipiscing elit.",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Estudar React 3",
+      description: "Loren ipsum dolor sit amet, consectetur adipiscing elit.",
+      completed: true,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function deleteTask(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+
+    setTasks(newTasks);
+  }
+
   return (
-    <dvi>
-      <h1>Gerenciador de Tarefa</h1>
-      <AddTask />
-      <Tasks />
-    </dvi>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefa
+        </h1>
+        <AddTask />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTask={deleteTask}
+        />
+      </div>
+    </div>
   );
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,30 @@
-function Tasks() {
-  return <h1>Tasks</h1>;
+import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
+
+function Tasks(props) {
+  console.log(props);
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-left text-white p-2 rounded-md w-full ${task.isCompleted ? "line-through" : ""}`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.onDeleteTask(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
