c18e3d558dd29bb7ab4182ece922d78b50923df7
rrsc-dev <reis.rrsc@outlook.com>
2026-02-03 14:11:40 -0300

    ajuste layout e botão voltar

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,4 +1,5 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
   const [searchParams] = useSearchParams();
@@ -6,10 +7,28 @@ function TaskPage() {
   const title = searchParams.get("title");
   const description = searchParams.get("description");
 
+  const navigate = useNavigate();
+
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6 text-slate-100">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
